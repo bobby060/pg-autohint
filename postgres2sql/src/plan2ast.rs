1364907983a0ef0abcead1bc05bb2c6c2b28eb81
Bobby Norwood <rnorwood@andrew.cmu.edu>
2025-03-21 19:19:39 +0000

    start outlining plan2ast logic

--- postgres2sql/src/plan2ast.rs
+++ postgres2sql/src/plan2ast.rs
@@ -7,6 +7,19 @@ use sqlparser::ast::*;
 ///
 ///
 pub fn plan2ast(_plan: PlanNode) -> Result<Query, String> {
+    // 1. Build body (SetExpr)
+
+    // 1.1 If select:
+    // 1.1.1 Build projection
+    // 1.1.2 Build from
+    // 1.1.3 Build having
+
+    // 1.2 If set, build of children recursively
+
+    // 2. Build order by
+
+    // 3. Build limit
+
     // Placeholder for the AST
     let ast = Query {
         with: None,
