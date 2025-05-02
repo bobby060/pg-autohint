344914325795477aa7a79ab8fa4eafaef3adac19
Bobby Norwood <rnorwood@andrew.cmu.edu>
2025-05-02 15:47:51 +0000

    fix order by rule tests

--- pg-autohint/src/hintengine/rules/order_by_incorrect_index_rule.rs
+++ pg-autohint/src/hintengine/rules/order_by_incorrect_index_rule.rs
@@ -108,7 +108,7 @@ mod test_order_by_incorrect_index_rule {
             Box::new(OrderByIncorrectIndex::new(&mut conn)),
             sql,
             false,
-        // );
+        );
         // assert_eq!(hints.size(), 1);
         // assert_eq!(hints.to_string().contains("IndexScan"), true,);
     }
